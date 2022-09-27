7516f681879ba20c20c0e5f6dbf5af927bf3a829
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-27 06:39:12 +0300

    COMPLETED: Custom Rendering

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,25 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
+    # render json: cheeses, methods: [:summary]
     render json: cheeses
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+
+    # built-in serialization options
+    # render json: cheese, only: [:id, :name, :price, :is_best_seller], methods: [:summary]
+
+    #error messaging
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      # render json: { error: "Data not found"}, status: :404
+      render json: { error: "Data not found"}, status: :not_found
+
+    end
   end
 
 end
